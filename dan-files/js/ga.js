0e2cf06957c35fb41ca9c1cb349f97c36888fd32
danv <dan.voronov@ymail.com>
2022-01-21 13:48:08 +0200

    ESC

--- dan-files/js/ga.js
+++ dan-files/js/ga.js
@@ -4,3 +4,8 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-5293921-1', 'auto');
 ga('send', 'pageview');
+
+document.onkeydown = (event) => {
+    if((event.keyCode ? event.keyCode : event.which) == '27')
+          window.location.href = '../'; //one level up
+};
